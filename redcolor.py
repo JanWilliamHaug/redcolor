bec18a6fddec2e51e096f68867d95b2909024da9
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2022-10-23 01:47:48 -0700

    first real commit of the demo program, searches for words written in red and makes a word document with them

--- redcolor.py
+++ redcolor.py
@@ -1,6 +1,9 @@
 import docx
 from docx import Document
 from docx.shared import RGBColor
+import string
+import re
+
 
 filename = "SRS_ACE_Pump_X00.docx"
 
@@ -15,7 +18,17 @@ def readtxt(filename):
     return fullText
 
 
+
+
+
 fullText = readtxt("SRS_ACE_Pump_X00.docx")
+
+s = ''.join(fullText)
+print(s)
+
+#o = s.split(']\w+')
+#print(o)
+
 
 #print str(fullText)[1:-1]
 #print(fullText)
@@ -23,8 +36,27 @@ fullText = readtxt("SRS_ACE_Pump_X00.docx")
 
 report = Document()
 #paragraph = report.add_paragraph(fullText[1])
-paragraph = report.add_paragraph(fullText)
+#paragraph = report.add_paragraph(fullText)
+#paragraph = report.add_paragraph(s)
+#report.save('report1.docx')
+
+
+w = (s.replace (']', ']\n'))
+paragraph = report.add_paragraph("SRS Ace Pump Document")
+paragraph = report.add_paragraph(w)
+
+
+paragraph = report.add_paragraph("PRS_new_pump.docx")
+
+filename2 = "PRS_new_pump.docx"
+
+fullText2 = readtxt("SRS_ACE_Pump_X00.docx")
+b = ''.join(fullText2)
+w = (b.replace (']', ']\n'))
+print(w)
+
+paragraph = report.add_paragraph(w)
+
 report.save('report1.docx')
 
-print(fullText)
 
