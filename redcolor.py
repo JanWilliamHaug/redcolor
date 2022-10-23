c5d06d4464a43a5543f8532dcd15174894e20094
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2022-10-22 19:56:11 -0700

    added filename variable

--- redcolor.py
+++ redcolor.py
@@ -1,5 +1,5 @@
 import docx
-from docx.shared import RGBColor
+from docx import Document
 from docx.shared import RGBColor
 
 filename = "SRS_ACE_Pump_X00.docx"
@@ -8,8 +8,8 @@ filename = "SRS_ACE_Pump_X00.docx"
 def readtxt(filename):
     doc = docx.Document(filename)
     fullText = []
-    for para in doc.paragraphs:
-        for run in para.runs:
+    for paras in doc.paragraphs:
+        for run in paras.runs:
             if run.font.color.rgb == RGBColor(255, 000, 000):
                 fullText.append(run.text)
     return fullText
@@ -17,4 +17,13 @@ def readtxt(filename):
 
 fullText = readtxt("SRS_ACE_Pump_X00.docx")
 
+#print str(fullText)[1:-1]
+#print(fullText)
+#print(fullText[5])
+
+#report = Document()
+#paragraph = report.add_paragraph(fullText[1])
+#report.save('report1.docx')
+
 print(fullText)
+
